9767c3a49df75378242499da53203ef9108cc7c1
Git1Gud <talhanaik2004@gmail.com>
2024-11-23 21:45:30 +0530

    signup fixed

--- frontend-web/src/app/(plain)/(authentication)/auth-advance/sign-up/components/SignUpForm.jsx
+++ frontend-web/src/app/(plain)/(authentication)/auth-advance/sign-up/components/SignUpForm.jsx
@@ -1,43 +1,61 @@
-import PasswordFormInput from '@/components/form/PasswordFormInput';
-import TextFormInput from '@/components/form/TextFormInput';
-import PasswordStrengthMeter from '@/components/PasswordStrengthMeter';
-import { currentYear, developedBy, developedByLink } from '@/context/constants';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { Button, FormCheck } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
+import PasswordFormInput from "@/components/form/PasswordFormInput";
+import TextFormInput from "@/components/form/TextFormInput";
+import PasswordStrengthMeter from "@/components/PasswordStrengthMeter";
+import { currentYear, developedBy, developedByLink } from "@/context/constants";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Button, FormCheck } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
 const SignUpForm = () => {
-  const [firstPassword, setFirstPassword] = useState('');
+  const [firstPassword, setFirstPassword] = useState("");
   const signUpSchema = yup.object({
-    email: yup.string().email('Please enter a valid email').required('please enter your email'),
-    password: yup.string().required('Please enter your password'),
-    confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords must match')
+    email: yup
+      .string()
+      .email("Please enter a valid email")
+      .required("please enter your email"),
+    password: yup.string().required("Please enter your password"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match"),
   });
-  const {
-    control,
-    handleSubmit,
-    watch,
-    getValues
-  } = useForm({
-    resolver: yupResolver(signUpSchema)
+  const { control, handleSubmit, watch, getValues } = useForm({
+    resolver: yupResolver(signUpSchema),
   });
   useEffect(() => {
     setFirstPassword(getValues().password);
-  }, [watch('password')]);
-  return <form className="mt-4" onSubmit={handleSubmit(() => {})}>
+  }, [watch("password")]);
+  return (
+    <form className="mt-4" onSubmit={handleSubmit(() => {})}>
       <div className="mb-3 text-start">
-        <TextFormInput name="email" control={control} containerClassName="input-group-lg" placeholder="Enter email" />
+        <TextFormInput
+          name="email"
+          control={control}
+          containerClassName="input-group-lg"
+          placeholder="Enter email"
+        />
         <small>We&apos;ll never share your email with anyone else.</small>
       </div>
       <div className="mb-3 position-relative">
-        <PasswordFormInput name="password" control={control} size="lg" placeholder="Enter new password" />
+        <PasswordFormInput
+          name="password"
+          control={control}
+          size="lg"
+          placeholder="Enter new password"
+        />
         <div className="mt-2">
           <PasswordStrengthMeter password={firstPassword} />
         </div>
       </div>
-      <PasswordFormInput name="confirmPassword" control={control} size="lg" containerClassName="mb-3" placeholder="Confirm password" />
+      <PasswordFormInput
+        name="confirmPassword"
+        control={control}
+        size="lg"
+        containerClassName="mb-3"
+        placeholder="Confirm password"
+      />
       <div className="mb-3 text-start">
         <FormCheck label="Keep me signed in" id="termAndCondition" />
       </div>
@@ -53,6 +71,7 @@ const SignUpForm = () => {
         </Link>
         All rights reserved
       </p>
-    </form>;
+    </form>
+  );
 };
 export default SignUpForm;
--- frontend-web/src/context/constants.js
+++ frontend-web/src/context/constants.js
@@ -1,8 +1,8 @@
 export const currency = '₹';
 export const currentYear = new Date().getFullYear();
-export const developedByLink = 'https://coderthemes.com/';
+export const developedByLink = 'https://github.com/ShubhamKarampure/ShikshaSangam';
 export const developedBy = 'Team Percetron';
-export const contactUs = 'mailto:support@coderthemes.com';
+export const contactUs = 'https://github.com/ShubhamKarampure/ShikshaSangam';
 export const buyLink = '';
 export const basePath = '';
 export const DEFAULT_PAGE_TITLE = 'ShikshaSangam';
