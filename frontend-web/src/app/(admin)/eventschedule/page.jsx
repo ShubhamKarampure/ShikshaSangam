f2dc5b4684b5394bb92bf9f07a54f4782a3b4d6d
Git1Gud <talhanaik2004@gmail.com>
2024-11-30 20:31:56 +0530

    Sidebar fix

--- frontend-web/src/app/(admin)/eventschedule/page.jsx
+++ frontend-web/src/app/(admin)/eventschedule/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import TopHeader from "@/components/layout/TopHeader";
+import Sidebar from "@/components/layout/SideBar";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div className="d-flex">
+      <Sidebar />
+      <div className="d-flex flex-column w-100">
+        <TopHeader />
+        <Container className="py-4" style={{ width: "60%"}}>
+          
+        </Container>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
