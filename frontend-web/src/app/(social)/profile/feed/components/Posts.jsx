5f53643cc4d0bd670731f6444e82c0837dfad2e9
Git1Gud <talhanaik2004@gmail.com>
2024-12-05 05:31:29 +0530

    meet fix

--- frontend-web/src/app/(social)/profile/feed/components/Posts.jsx
+++ frontend-web/src/app/(social)/profile/feed/components/Posts.jsx
@@ -1,11 +1,12 @@
 import { Card, CardBody, CardFooter, CardHeader, Dropdown, DropdownDivider, DropdownItem, DropdownMenu, DropdownToggle } from 'react-bootstrap';
 import { BsBookmark, BsBookmarkCheck, BsChatFill, BsEnvelope, BsFlag, BsHeart, BsHeartFill, BsLink, BsPencilSquare, BsPersonX, BsReplyFill, BsSendFill, BsShare, BsSlashCircle, BsThreeDots, BsXCircle } from 'react-icons/bs';
-import logo13 from '@/assets/images/logo/13.svg';
-import PostCard from '@/components/cards/PostCard';
-import { getAllFeeds } from '@/helpers/data';
-import { Link } from 'react-router-dom';
-import { useFetchData } from '@/hooks/useFetchData';
 import Post3 from '@/app/(social)/feed/(container)/home/components/FeedComponents/Post3';
+import { getAllFeed } from "@/api/feed";
+import PostCard from "@/components/cards/PostCard";
+import LoadMoreButton from "@/app/(social)/feed/(container)/home/components/LoadMoreButton.jsx";
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ActionMenu = ({
   name
@@ -58,133 +59,54 @@ const ActionMenu = ({
     </Dropdown>;
 };
 const Posts = () => {
-  const allPosts = useFetchData(getAllFeeds);
-  return <>
-      {allPosts?.slice(0, 1).map((post, idx) => <Post3 {...post} key={idx} />)}
-      <Card>
-        <div className="border-bottom">
-          <p className="small mb-0 px-4 py-2">
-            <BsHeartFill className="text-danger pe-1" size={18} />
-            Sam Lanson likes this post
-          </p>
-        </div>
-        <CardHeader className="border-0 pb-0">
-          <div className="d-flex align-items-center justify-content-between">
-            <div className="d-flex align-items-center">
-              <div className="avatar me-2">
-                <span role="button">
-                  
-                  <img className="avatar-img rounded-circle" src={logo13} alt="image" />
-                </span>
-              </div>
-              <div>
-                <h6 className="card-title mb-0">
-                  
-                  <Link to=""> Apple Education </Link>
-                </h6>
-                <p className="mb-0 small">9 November at 23:29</p>
-              </div>
-            </div>
-            <ActionMenu />
-          </div>
-        </CardHeader>
-        <CardBody className="pb-0">
-          <p>
-            Find out how you can save time in the classroom this year. Earn recognition while you learn new skills on iPad and Mac. Start recognition
-            your first Apple Teacher badge today!
-          </p>
-          <ul className="nav nav-stack pb-2 small">
-            <li className="nav-item">
-              <Link className="nav-link active text-secondary " to="">
-                
-                <span className="icon-xs bg-danger text-white rounded-circle me-1">
-                  <BsHeartFill size={8} />
-                </span>
-                Louis, Billy and 126 others
-              </Link>
-            </li>
-            <li className="nav-item ms-sm-auto">
-              <Link className="nav-link" to="">
-                
-                <BsChatFill className="pe-1" size={18} />
-                Comments (12)
-              </Link>
-            </li>
-          </ul>
-        </CardBody>
-        <CardFooter className="py-3">
-          <ul className="nav nav-fill nav-stack small">
-            <li className="nav-item">
-              <Link className="nav-link mb-0 active" to="">
-                
-                <BsHeart size={18} className="pe-1" />
-                Liked (56)
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link mb-0" to="">
-                
-                <BsChatFill size={18} className="pe-1" />
-                Comments (12)
-              </Link>
-            </li>
-            <Dropdown className="nav-item">
-              <DropdownToggle as="a" className="nav-link mb-0 content-none cursor-pointer" id="cardShareAction" data-bs-toggle="dropdown" aria-expanded="false">
-                <BsReplyFill size={17} className="flip-horizontal ps-1" />
-                Share (3)
-              </DropdownToggle>
+  const [allPosts, setAllPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-              <DropdownMenu className="dropdown-menu-end" aria-labelledby="cardShareAction">
-                <li>
-                  <DropdownItem>
-                    
-                    <BsEnvelope size={22} className="fa-fw pe-2" />
-                    Send via Direct Message
-                  </DropdownItem>
-                </li>
-                <li>
-                  <DropdownItem>
-                    
-                    <BsBookmarkCheck size={22} className="fa-fw pe-2" />
-                    Bookmark
-                  </DropdownItem>
-                </li>
-                <li>
-                  <DropdownItem>
-                    
-                    <BsLink size={22} className="fa-fw pe-2" />
-                    Copy link to post
-                  </DropdownItem>
-                </li>
-                <li>
-                  <DropdownItem>
-                    
-                    <BsShare size={22} className="fa-fw pe-2" />
-                    Share post via …
-                  </DropdownItem>
-                </li>
-                <li>
-                  <DropdownDivider />
-                </li>
-                <li>
-                  <DropdownItem>
-                    
-                    <BsPencilSquare size={22} className="fa-fw pe-2" />
-                    Share to News Feed
-                  </DropdownItem>
-                </li>
-              </DropdownMenu>
-            </Dropdown>
-            <li className="nav-item">
-              <Link className="nav-link mb-0" to="">
-                
-                <BsSendFill size={17} className="pe-1" />
-                Send
-              </Link>
-            </li>
-          </ul>
-        </CardFooter>
-      </Card>
-    </>;
+  useEffect(() => {
+    const fetchFeeds = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getAllFeed();
+        console.log(data);
+
+        setAllPosts(data);
+      } catch (err) {
+        console.error("Error fetching feeds:", err);
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchFeeds();
+  }, []);
+
+  if (error) {
+    return <p>Error loading feeds: {error.message}</p>;
+  }
+  return (
+    <>
+    {isLoading ? (
+      Array.from({ length: 5 }).map((_, idx) => (
+        <div
+          key={idx}
+          className="skeleton-container"
+          style={{ height: "150px", width: "100%" }} // Match the card height
+        >
+          <Skeleton height={50} width={50} circle={true} /> {/* Avatar */}
+          <Skeleton height={20} width={`80%`} /> {/* Title */}
+          <Skeleton height={15} width={`60%`} /> {/* Description */}
+        </div>
+      ))
+    ) : allPosts.length > 0 ? (
+      allPosts.map((post, idx) => <Post3 {...post} key={idx} />)
+    ) : (
+      <p>No posts available.</p>
+    )}
+
+    <LoadMoreButton />
+  </>
+  )
 };
 export default Posts;
